f1cba07cf0aff67f0a7c96384b3c3c2325d162a3
Colton Briggs <wolflink013@gmail.com>
2025-07-23 09:05:10 -0700

    Done

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CarLotSimulator;
+
+public class Car
+{
+    public string Year {get; set; }
+    public string Make {get; set; }
+    public string Model {get; set; }
+    public string EngineNoise {get; set; }
+    public string HonkNoise {get; set; }
+    public bool IsDriveable {get; set; }
+
+    public void MakeEngineNoise(string engineNoise)
+    {
+        Console.WriteLine($"This car goes {engineNoise}");
+    }
+
+    public void MakeHonkNoise(string honkNoise)
+    {
+        Console.WriteLine($"The horn of this car gose {honkNoise}");
+    }
+
+    public Car(string year, string make, string model, string engineNoise, string honkNoise)
+    {
+        Year = year;
+        Make = make;
+        Model = model;
+        EngineNoise = engineNoise;
+        HonkNoise = honkNoise;
+    }
+
+    public Car()
+    {
+        
+    }
+    
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -4,8 +4,43 @@ namespace CarLotSimulator
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+             Car car1 = new Car("2020","Honda", "Civic", "RagggggRaaahhhggg","BEEpBepp");
+
+
+             Car car2 = new Car()
+             {
+                 Year = "2019",
+                 Make = "Toyota",
+                 Model = "Corolla", 
+                 EngineNoise = "vreeeerreee",
+                 HonkNoise = "HonKHonnk"
+             };
+             
+             Car car3 = new Car();
+             car3.Year = "2018";
+             car3.Make = "Ford";
+             car3.Model = "Fusion";
+             car3.EngineNoise = "Rrrrrrrrrr";
+             car3.HonkNoise = "MeeepMeeep";
+             
+             car1.MakeEngineNoise(car1.EngineNoise); 
+             car2.MakeEngineNoise(car2.EngineNoise); 
+             car3.MakeEngineNoise(car3.EngineNoise); 
+             
+             car1.MakeHonkNoise(car1.HonkNoise);
+             car2.MakeHonkNoise(car1.HonkNoise);
+             car3.MakeHonkNoise(car3.HonkNoise);
+             
+
+
+        }
+
+
+
+    }
             //TODO
 
             //Create a seperate class file called Car
@@ -17,17 +52,21 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            
+            
 
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
-
+            
+            
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-        }
-    }
+        
+    
+
 }
